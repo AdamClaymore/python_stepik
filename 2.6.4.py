64eef6fc5bbf8241e268e3029156e92344a68ab0
Artem Konnov <adamclaymore679@gmail.com>
2020-05-16 23:28:15 +0500

    done

--- 2.6.4.py
+++ 2.6.4.py
@@ -3,27 +3,22 @@
 start = 1
 a = int(input())
 b = [[0 for j in range(a)] for i in range(a)]
-
-j = 0
-
-for i in range(a):
-        b[j][i] = start
+b[a // 2][a // 2] = a * a
+for r in range(a // 2):
+    j = 0
+    for i in range(a - r - r):
+        b[j + r][i + r] = start
         start = start + 1
-for j in range(a-1):
-         b[j+1][i] = start
-         start = start + 1
-
-for k in range(a-1):
-         b[a-1][a - k - 2] = start
-         start = start + 1
-for m in range(a-2):
-         b[a-2-m][a-a] = start
-         start = start + 1
-
-      #print(b[j][i], end=" ")
-for y in range(5):
-    for j in range(5):
-        print(b[y][j], end="\t")
-    print("\n")
-
-
+    for j in range(a - 1 - r - r):
+        b[j + 1 + r][i + r] = start
+        start = start + 1
+    for k in range(a - 1 - r - r):
+        b[a - 1 - r][a - k - 2 - r] = start
+        start = start + 1
+    for m in range(a - 2 - r - r):
+        b[a - 2 - m - r][a - a + r] = start
+        start = start + 1
+for y in range(a):
+    for j in range(a):
+        print(b[y][j], end=" ")
+    print("")
